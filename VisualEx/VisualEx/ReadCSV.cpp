7308325a973e508d7a9e1833eaffa57cdb943f51
ST <Sthou3@gmail.com>
2014-08-28 12:57:05 -0700

    Found occurrences and position of commas

--- VisualEx/VisualEx/ReadCSV.cpp
+++ VisualEx/VisualEx/ReadCSV.cpp
@@ -1,46 +1,68 @@
-//
-//  main.cpp
-//  VisualEx
-//
-//  Created by ST on 8/26/14.
-//  Copyright (c) 2014 ST. All rights reserved.
-//
+//============================================================================
+// Name        : VisualExPC.cpp
+// Author      : ST
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
+#include <cstdlib> // for exit functions
 #include <iostream>
 #include <fstream> // stream class to both read and write from/to files
+#include <string>
 
 using namespace std;
 
 int main()
 {
     char data[100];
-    
-    
+
+
     fstream file("CSVStats.csv");
-    
-  
+
+
     if (!file)
     { cerr << " File Could Not Open" ;
-      exit (1);
+      exit(1);
     }
 
-    
+
     int i = 0;
+    int commapp = 0;
+    int c = 0;
+    int array [328328];
     // Read from file
      while (file)
      {
      string input;
+     string charmatch = (",");
      getline(file, input);
-     cout<< input <<endl;
-         i++;
-     
+
+//11 is the first occurance
+
+    for (i = input.find(charmatch, 0 ); i != string::npos; i = input.find(charmatch, i))
+    {
+
+    	if (input.find(charmatch,i))
+    	{
+    		array[c] = i;
+    		c++;
+    	}
+    	commapp++;
+    	i++;
+    }
+
+    for (int b = 0; b <100; b++){
+
+    	cout<<array[b]<<endl;
+    }
      }
-    
+
     ;;
-    
+
     return 0;
-    
-    
-    
+
+
+
 }
 
