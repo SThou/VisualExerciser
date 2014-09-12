caee59f61279eb19ad52ad98b7a5750aea95b981
ST <ST@STs-MacBook-Pro-4.local>
2014-09-11 19:00:03 -0700

    Header file for converting Stats main file to weekly stats information. Array copying from record 1 to record 2 does not work properly. It seems that record2 is reading in a weird way.

---
+++ VisualEx/VisualEx/RWWeeklyStats.cpp
@@ -0,0 +1,120 @@
+//
+//  RWWeeklyStats.cpp
+//  VIsualEx
+//
+//  Created by ST on 9/10/14.
+//  Copyright (c) 2014 ST. All rights reserved.
+//  Description: this will take the userstat filled out by user in ReadCSV.CPP
+//               and log data to a weekly file used for calculation on stats.
+//               User press log data will write data to the ReadCSV.CPP
+
+#include "RWWeeklyStats.h"
+
+
+using namespace std;
+
+void transStats(){
+    
+    fstream FileIn;
+    FileIn.open("MainStatsFile.csv"); // open edited main user stat file
+    
+    if (!FileIn)
+    { cerr << " File Could Not Open" ;
+        exit(1);
+    }
+    
+    
+    // represent dataset as two dimensional array to be read into
+    string record[12][5]; // 12x5 record
+    int row, col; // used to traverse
+    
+    
+    string fileparsed[200]; // parsed data to
+    
+    
+    
+    while (FileIn)
+    {
+        FileIn.seekg(0, FileIn.end);
+        long flength = FileIn.tellg();
+        FileIn.seekg(0, FileIn.beg);
+        
+        char *buffer = new char[flength];
+        FileIn.read(buffer, flength+1);
+        
+        // token setup
+        char *tokenptr;
+        tokenptr = strtok(buffer, ",");
+        
+        int x = 0;
+        while (tokenptr != NULL) // parse token and convert data into strings
+        {
+            fileparsed[x] = tokenptr;
+            x++;
+            tokenptr = strtok(NULL, ","); // next token
+        } // end while parsing loop
+        
+    } // end file while loop
+    
+    
+    
+    // Read data into array
+    int i = 0;
+    for(row = 0; row < 12; row++)
+        for(col =0; col<5; col++){
+            record[row][col] = fileparsed[i];
+            i++;
+        } // end for read data for loop
+    
+
+    // write a new heading manually
+    
+    string record2[12][5]; // 12x5 record to copy original file into and write to
+    
+    for (int i = 0; i <5 ; i++){
+        stringstream appended;
+        appended <<"Week"<<i+1;
+        string done;
+        done = appended.str();
+        record2[0][i] = done;
+        
+    }
+    // WHY!!!!!! is it doing this
+    uint32_t x;
+    for(x = 0; x<12; x++){ // start below header
+     //   for(int y = 0; y<5; y++){ // clear all other data
+        cout<<record[x][5]<<endl;
+           // record2[x][y] = " x " ;
+            record2[x][0] = record[x][5]; // move userstat in the fifth column to the begging of the first
+    }
+    
+        
+        
+ //   }
+    
+    
+    
+
+        ofstream outputfile;
+        outputfile.open("WeeklyStats.csv");
+        int rows, cols;
+        for(rows = 0; rows< 12; rows++){
+            for(cols=0; cols<5; cols++){
+                outputfile << record2[rows][cols]<<",";
+                if(rows == 0 && cols ==4){
+                    outputfile<<"\n";
+                }
+                
+            } // end for read data for loop
+            
+            if(5 == cols && 0 != rows ){
+                outputfile<<"\n";
+            }
+        }
+        
+        
+        outputfile.close();
+    
+  
+
+} // end function
---
+++ VisualEx/VisualEx/RWWeeklyStats.h
@@ -0,0 +1,22 @@
+//
+//  RWWeeklyStats.h
+//  VIsualEx
+//
+//  Created by ST on 9/10/14.
+//  Copyright (c) 2014 ST. All rights reserved.
+//
+
+#ifndef __VIsualEx__RWWeeklyStats__
+#define __VIsualEx__RWWeeklyStats__
+
+#include <fstream> // stream class to read and write from/to files
+#include <iostream> // cerr
+#include <sstream>
+
+
+
+
+void transStats();
+
+
+#endif /* defined(__VIsualEx__RWWeeklyStats__) */
