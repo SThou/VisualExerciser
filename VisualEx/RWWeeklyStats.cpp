0682ac77814f0d361ccd03ac7efe27debe1754e0
Harish <harish_kothandapani@yahoo.com>
2014-09-12 19:25:08 +0530

    CSV weeklystats output fixed
    debugged and fixed the errors that were giving trouble to properly
    outputting the last column of the main file to the first column of
    weekly stats

---
+++ VisualEx/RWWeeklyStats.cpp
@@ -0,0 +1,131 @@
+//
+//  RWWeeklyStats.cpp
+//  VIsualEx
+//
+//  Created by ST on 9/10/14.
+//  Copyright (c) 2014 ST. All rights reserved.
+//  Description: this will take the userstat filled out by user in ReadCSV.CPP
+//               and log data to a weekly file used for calculation on stats.
+//               User press log data will write data to the ReadCSV.CPP
+
+#include "RWWeeklyStats.h"
+
+
+using namespace std;
+
+void transStats(){
+
+    fstream FileIn;
+    FileIn.open("MainStatsFile.csv"); // open edited main user stat file
+
+    if (!FileIn)
+    { cerr << " File Could Not Open" ;
+        exit(1);
+    }
+
+
+    // represent dataset as two dimensional array to be read into
+    string record[12][5]; // 12x5 record
+    int row, col; // used to traverse
+
+
+    string fileparsed[200]; // parsed data to
+    stringstream ss;
+    string converted;
+
+
+    while (FileIn)
+    {
+        FileIn.seekg(0, FileIn.end);
+        int flength = FileIn.tellg();
+        FileIn.seekg(0, FileIn.beg);
+
+        char *buffer = new char[flength];
+        FileIn.read(buffer, flength+1);
+
+        // token setup
+        char *tokenptr;
+        tokenptr = strtok(buffer, ",");
+
+        int x = 0;
+        while (tokenptr != NULL) // parse token and convert data into strings
+        {
+            ss << tokenptr;
+            if(x >= 1) //HK: Used our original method of parsing token data to avoid conflicts
+            {
+            ss >> converted;
+            //fileparsed[x] = tokenptr;
+            fileparsed[x] = converted;
+            }
+            x++;
+            tokenptr = strtok(NULL, ","); // next token
+        } // end while parsing loop
+
+    } // end file while loop
+
+
+
+    // Read data into array
+    int i = 0;
+    for(row = 0; row < 12; row++)
+        for(col =0; col<5; col++){
+            record[row][col] = fileparsed[i+1]; //HK: fileparsed[i] would have included the 0(NULL) thats why it was messing up
+            //cout << record[row][col] << endl;
+            i++;
+        } // end for read data for loop
+
+
+    // write a new heading manually
+
+    string record2[12][5]; // 12x5 record to copy original file into and write to
+
+    for (int i = 0; i <5 ; i++){
+        stringstream appended;
+        appended <<"Week"<<i+1;
+        string done;
+        done = appended.str();
+        record2[0][i] = done;
+
+    }
+    //uint32_t x;
+    int rowcout; //HK: not using "x" because you declared earlier in program
+    for( rowcout= 0; rowcout<12; rowcout++){ // start below header
+       // for(int y = 0; y<5; y++){ // clear all other data
+       if(rowcout!=0) //HK: i assume you dont want "Userstats" replacing week1 haha
+       {
+        cout<<record[rowcout][4]<<endl; //HK: changed column to 4... because theres no 6th column(array starts from 0)
+           // record2[x][y] = " x " ;
+            record2[rowcout][0] = record[rowcout][4]; // move userstat in the fifth column to the begging of the first
+       }
+    }
+
+
+
+ //   }
+
+
+
+
+        ofstream outputfile;
+        outputfile.open("WeeklyStats.csv");
+        int rows, cols;
+        for(rows = 0; rows< 12; rows++){
+            for(cols=0; cols<5; cols++){
+                outputfile << record2[rows][cols]<<",";
+                if(rows == 0 && cols ==4){
+                    outputfile<<"\n";
+                }
+
+            } // end for read data for loop
+
+            if(5 == cols && 0 != rows ){
+                outputfile<<"\n";
+            }
+        }
+
+
+        outputfile.close();
+
+
+
+} // end function
---
+++ VisualEx/readcsv.cpp
@@ -0,0 +1,177 @@
+//============================================================================
+// Name        : VisualExPC.cpp
+// Author      : ST
+// Version     :
+// Copyright   :
+// Description : This is the main file to read the start CSV file and write user input to the CSV file.
+//============================================================================
+
+#include <cstdlib> // for exit functions
+#include <iostream>
+#include <fstream> // stream class to both read and write from/to files
+#include <cstring>
+#include <cstdio>
+#include <stdio.h>
+#include <sstream>
+#include <iomanip> // string printout adjustment
+#include "RWWeeklyStats.h"
+
+using namespace std;
+
+/*
+// functions for reading data structure to console
+void readdata(string records[][5]){
+    int row = 0, col = 0;
+    for(row = 0; row < 12; row++)
+        for(col = 0; col<5; col++){
+            cout<<setw(7)<<records[row][col]<<"|";
+   
+        }
+    cout << endl;
+}
+
+//void UserStatsAdjustment(string records[][5], inputcol, inputrow) // will have to be adjusted depending on interface method
+void UserStatsAdjustment(string records[][5]){ //A user selects a specific row & col to input values
+    
+        string input;
+        int inputcol = 4; // only user can adjust user stats
+        int inputrow; // limited to row 1+
+        char exit = 0; // exit value
+    
+    
+        while(exit != 'N')
+        {
+          
+        cout << "Enter in row ";
+        cin >> inputrow;
+    
+
+        if(0 == inputrow ){
+        cout<< "This row cannot be selected" <<endl;
+        cout<< "Enter in a new row" << endl;
+            cin >>inputrow;
+            } // end if
+    
+        cout<< "Enter in user stat" <<endl;
+        cin >> input;
+    
+        records[inputrow][inputcol] = input;
+        
+        cout<< "Enter another( Y )  or exit (N)?"<<endl;
+        cin>>exit;
+            
+        }
+    
+    
+}
+
+void writetofile(string records[][5]){
+    ofstream outputfile;
+    outputfile.open("MainStatsFile.csv");
+    int row, col;
+    for(row = 0; row < 12; row++){
+        for(col=0; col<5; col++){
+            outputfile << records[row][col]<<",";
+        } // end for read data for loop
+       
+        if(5 == col && 0 != row ){
+        outputfile<<"\n";
+        }
+    }
+    
+    
+    outputfile.close();
+    
+}
+
+
+//record of one weight
+struct record{
+    
+	string userweighthead; // heading for user weight
+	string userweight;
+	string exercise[30]; // individual exercise
+	char malenationalstats;
+};
+
+
+
+int main(void)
+{
+    // Begin reading data from file to parsing to data structure
+    fstream file("CSVStats.csv");
+    
+    
+    if (!file)
+    { cerr << " File Could Not Open" ;
+        exit(1);
+    }
+    
+    // represent dataset as two dimensional array to be read into
+    string excelrecord[12][5]; // 12x5 record
+    int row, col; // used to traverse
+    
+    
+    string fileparsed[200]; // parsed data to
+    
+
+    
+    while (file)
+    {
+        file.seekg(0, file.end);
+        long flength = file.tellg();
+        file.seekg(0, file.beg);
+        
+        char *buffer = new char[flength];
+        file.read(buffer, flength+1);
+        
+        // token setup
+        char *tokenptr;
+        tokenptr = strtok(buffer, ",");
+        
+        int x = 0;
+        while (tokenptr != NULL) // parse token and convert data into strings
+        {
+                fileparsed[x] = tokenptr;
+                x++;
+            tokenptr = strtok(NULL, ","); // next token
+        } // end while parsing loop
+        
+    } // end file while loop
+
+    
+    
+    // Read data into array
+    int i = 0;
+    for(row = 0; row < 12; row++)
+        for(col =0; col<5; col++){
+            excelrecord[row][col] = fileparsed[i];
+            i++;
+        } // end for read data for loop
+   
+    
+    
+    
+
+    
+    // print back data
+    UserStatsAdjustment(excelrecord); // write to initial file
+    
+    readdata(excelrecord);
+    writetofile(excelrecord); // write to v1 of the stats file.
+
+    
+    
+} // end main
+
+*/
+
+int main(){ // test .h
+    transStats();
+}
+
+
+
+
+
+
